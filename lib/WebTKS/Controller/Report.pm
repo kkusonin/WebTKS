10a91e259945746cc3030c3c92b8bf57fef5ca2b
kkusonin <kusonin@gmail.com>
2014-04-16 10:10:41 +0400

    minor changing in  report export exporting

--- lib/WebTKS/Controller/Report.pm
+++ lib/WebTKS/Controller/Report.pm
@@ -20,51 +20,56 @@ Catalyst Controller.
 
 
 sub report_base : Chained('/') PathPart('reports') CaptureArgs(0) {
-	my ($self, $c) = @_;
-	$c->stash( bootstrap => 1 );
+    my ($self, $c) = @_;
+    $c->stash( bootstrap => 1 );
 }
 
 sub report_form : GET Chained('report_base') PathPart('') Args(0) {
-	my ($self, $c) = @_;
-	
-	$c->stash(
-		action   => $c->uri_for($c->controller->action_for('create')),
-		template => 'reports/index.tt',
-	);
+    my ($self, $c) = @_;
+    
+    $c->stash(
+        action   => $c->uri_for($c->controller->action_for('create')),
+        template => 'reports/index.tt',
+    );
 }
 
 sub create : POST Chained('report_base') PathPart('') Args(0) {
-	my ($self, $c) = @_;
-	
-	my $date_parser = DateTime::Format::Strptime->new(pattern => "%Y-%m-%d %H:%M:%S");
-	
-	my $start = $date_parser->parse_datetime($c->req->params->{start});
-	my $end = $date_parser->parse_datetime($c->req->params->{end});
-	
-	my @report_rows = ($c->req->params->{selection_type} == 1) ?
-							map { order_to_report($_) } $c->model('DB::TKS::Order')->entry_interval($start, $end)
-						  : map { order_to_report($_) } $c->model('DB::TKS::Order')->update_interval($start, $end);
-	$c->stash(
-		report_data => \@report_rows,
-		template => 'report.tt',
-	);
-	
-	$c->res->header('Content-Disposition', qq[attachment; filename="report.xls"]);
-	$c->forward('WebTKS::View::Excel');
+    my ($self, $c) = @_;
+    
+    my $date_parser = DateTime::Format::Strptime->new(pattern => "%d/%m/%Y %H:%M");
+      
+    eval {
+        my $start = $date_parser->parse_datetime($c->req->params->{start});
+        my $end = $date_parser->parse_datetime($c->req->params->{end});
+    
+        my @report_rows = ($c->req->params->{selection_type} == 1) ?
+                            map { order_to_report($_) } $c->model('DB::TKS::Order')->entry_interval($start, $end)
+                          : map { order_to_report($_) } $c->model('DB::TKS::Order')->update_interval($start, $end);
+    
+        $c->stash(
+            report_data => \@report_rows,
+            template => 'report.tt',
+        );
+    };
+    if ($@) {
+        $c->detach('report_form');
+    }
+    $c->res->header('Content-Disposition', qq[attachment; filename="report.xls"]);
+    $c->forward('WebTKS::View::Excel');
 }
 
 sub order_to_report {
 {
-		entry_date => (defined $_[0]->entry_time) ? $_[0]->entry_time->strftime("%Y-%m-%d") : '',
-		entry_time => (defined $_[0]->entry_time) ? $_[0]->entry_time->strftime("%H:%M:%S") : '',
-		phone_number => $_[0]->phone_number,
-		user => $_[0]->user,
-		full_name => $_[0]->full_name,
-		lead_id => $_[0]->lead_id,
-		status => $_[0]->status,
-		update_date => (defined $_[0]->update_time) ? $_[0]->update_time->strftime("%Y-%m-%d") : '',
-		update_time => (defined $_[0]->update_time) ? $_[0]->update_time->strftime("%H:%M:%S") : '',
-	}
+        entry_date => (defined $_[0]->entry_time) ? $_[0]->entry_time->strftime("%Y-%m-%d") : '',
+        entry_time => (defined $_[0]->entry_time) ? $_[0]->entry_time->strftime("%H:%M:%S") : '',
+        phone_number => $_[0]->phone_number,
+        user => $_[0]->user,
+        full_name => $_[0]->full_name,
+        lead_id => $_[0]->lead_id,
+        status => $_[0]->status,
+        update_date => (defined $_[0]->update_time) ? $_[0]->update_time->strftime("%Y-%m-%d") : '',
+        update_time => (defined $_[0]->update_time) ? $_[0]->update_time->strftime("%H:%M:%S") : '',
+    }
 }
 
 =encoding utf8
